14bf889c642f72d69ed2ef8ba6a95de0218cd53e
ashraf <mohamedxman859@gmail.com>
2023-05-14 16:33:29 +0300

    data base setup

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-
+const Workout = require('../models/workout')
 //get all workouts
 router.get('/', (req, res)=>{
     res.json({msg: 'Get all workouts'})
@@ -9,6 +9,28 @@ router.get('/', (req, res)=>{
 //get workout by id 
 router.get('/:id', (req, res)=>{
     res.json({'id': req.params.id})
+})
+
+//create new workout
+router.post('/', async(req, res)=>{
+    const {title, load, reps} = req.body
+    try{
+        const workout = await Workout.create({title, load, reps})
+        res.status(200).json(workout)
+    }catch(error){
+        res.status(400).json({error: error.message})
+    }
+    res.json({msg: 'create a new workout'})
+})
+
+//delete workout
+router.delete('/:id', (req, res)=>{
+    res.json({msg: 'delete a new workout'})
+})
+
+//update new workout
+router.patch('/:id', (req, res)=>{
+    res.json({msg: 'update a workout'})
 })
 
 module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,21 @@
 const express = require('express')
+require('dotenv').config()
 const workoutRoutes = require('./routes/workouts')
 const app = express()
+const mongoose = require('mongoose')
 
+app.use(express.json())
 app.use((req, res, next)=>{
     console.log(req.path)
     next()
 })
 app.use('/api/workouts',workoutRoutes)
-app.listen(4000, ()=>{
-    console.log('listening on port 4000')
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    app.listen(4000, ()=>{
+        console.log('listening on port 4000')
+        console.log('connected to db')
+    })
+}).catch((error)=>{
+    console.log(error)
 })
